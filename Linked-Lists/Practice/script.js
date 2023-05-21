b42fbdad6af0e820104e59e3f143d04a4be01370
Alexander <lixo.mueller@gmail.com>
2023-05-21 09:55:39 -0600

    last updated 05.21.2023

--- Linked-Lists/Practice/script.js
+++ Linked-Lists/Practice/script.js
@@ -32,11 +32,42 @@ class LinkedList {
         }
         return this.locomotive.trainCart;
     }
+
+    findSumOfTrainCarts() {
+        let trainCartInspector = this.locomotive;
+        let sumOfTrainCarts = 0;
+
+        while(trainCartInspector !== null) {
+            sumOfTrainCarts++;
+
+            if(trainCartInspector.nextTrainCart === null) {
+                break;
+            }
+            trainCartInspector = trainCartInspector.nextTrainCart;
+        }
+        return sumOfTrainCarts;
+    }
+
+    doesTrainContainNodeTrainCart(trainCartValue) {
+        let trainCartInspector = this.locomotive;
+        let didTheInspectorFindTheTrainCartValue = false;
+        while (trainCartInspector !== null) {
+            if (trainCartInspector.trainCart === trainCartValue) {
+                didTheInspectorFindTheTrainCartValue = true;
+                break;
+            }
+            trainCartInspector = trainCartInspector.nextTrainCart;
+        }
+        return didTheInspectorFindTheTrainCartValue;
+    }
 }
 
 const newlinkedList = new LinkedList;
 newlinkedList.addCartToFront("1");
 newlinkedList.addCartToFront("2");
 newlinkedList.addCartToFront("3");
+newlinkedList.addCartToFront("4");
 newlinkedList.RemoveFront();
-console.log(newlinkedList.displayLocomotiveTrainCartAtFront());
+//console.log(newlinkedList.displayLocomotiveTrainCartAtFront());
+//console.log(newlinkedList.findSumOfTrainCarts());
+console.log(newlinkedList.doesTrainContainNodeTrainCart("4"));
